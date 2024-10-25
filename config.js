ac560e62e4474a83a676a93fb275fcf627dd8ef7
⚜️ ╰‿╯ MR Pabasara Malinda  ད★࿐ <142970254+CyberPabuZa0@users.noreply.github.com>
2024-10-25 18:32:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=PjgSnITR#j9RQalRwhSknK1ND6Mo6gidFVRZtlDPd-YWSNCxMAnI",
+MONGODB: process.env.MONGODB || "mongodb+srv://pabasara:pabasara@123@cluster0.om16l.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
